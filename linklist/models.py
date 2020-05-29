6486c36ef9c89d3ebe2dd05a437a36b7cccf5614
Jeremy Wei <jeremy.wei@flatiron.com>
2020-05-29 18:45:35 -0400

    create models for link and linklist and did migrations

--- linklist/models.py
+++ linklist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Link(models.Model):
+    link = models.URLField(max_length=300)
+    date_added = models.DateTimeField()
+
+
+class LinkList(models.Model):
+    title = models.CharField(max_length=100)
+    links = models.ManyToManyField(Link)
