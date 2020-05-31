c6474fdd2c7fcf7d5dbbb25f4f9979a2034708ff
Jeremy Wei <jeremy.wei@flatiron.com>
2020-05-31 18:18:10 -0400

    Added views for lists and links

--- linklist/views.py
+++ linklist/views.py
@@ -1,5 +1,27 @@
 from django.http import HttpResponse
+from django.views.generic import ListView
+from linklist.models import Link, LinkList
+from django.template import loader
 
-# Create your views here.
+
+# Index view for list of link lists
 def index(request):
-    return HttpResponse("See a list of your links here")
+    lists = LinkList.objects.all()
+    template = loader.get_template('linklist/index.html')
+    context = {
+            'lists': lists,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+# Detail view for a list, which displays a list of links
+def detail(request, linklist_id):
+    # TODO: need to filter by linklist_id
+    links = Link.objects.all()
+    print(links)
+    template = loader.get_template('linklist/detail.html')
+    context = {
+            'links': links,
+    }
+    return HttpResponse(template.render(context, request))
+
