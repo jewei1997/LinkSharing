c55e10ea80b238237ef40bb7f87c840d4c5c192c
Jeremy Wei <jeremy.wei@flatiron.com>
2020-05-31 22:38:35 -0400

    added title to link and did db migrations

--- linklist/models.py
+++ linklist/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Link(models.Model):
+    title = models.CharField(max_length=200)
     link = models.URLField(max_length=300)
     date_added = models.DateTimeField()
 
