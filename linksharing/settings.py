84cbac5a2f966edc14f632eeec9e017c678f5b33
Jeremy Wei <jeremy.wei@flatiron.com>
2020-06-04 23:31:15 -0400

    added password reset but only once the user is already logged in

--- linksharing/settings.py
+++ linksharing/settings.py
@@ -122,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
