2b70e98c2d54f5d5735e388a61e5c69fce308fc9
Jeremy Wei <jeremy.wei@flatiron.com>
2020-06-06 16:11:10 -0400

    Associating linklists with users

--- linklist/models.py
+++ linklist/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 
 class LinkList(models.Model):
     title = models.CharField(max_length=100)
     date_created = models.DateTimeField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True,
+                             null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
