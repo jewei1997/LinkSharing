037b656f269cd60f24a8bf06a7608640384be61c
Jeremy Wei <jeremy.wei@flatiron.com>
2020-06-09 23:00:54 -0400

    Got basic test to work

--- linklist/tests.py
+++ linklist/tests.py
@@ -1,5 +1,6 @@
-# from django.test import TestCase
+from datetime import date
 from linklist.models import Link, LinkList
+from django.test import TestCase
 
 
 def create_linklist(title, date_created):
@@ -9,3 +10,15 @@ def create_linklist(title, date_created):
 def create_link(title, link, date_added, linklist):
     return Link(title=title, link=link,
                 date_added=date_added, linklist=linklist)
+
+def test_hi():
+    assert 3 + 1 == 4
+
+
+class LinklistModelTests(TestCase):
+    def test_create_linklist(self):
+        title = "my list"
+        date_created = date(2020, 6, 5)
+        ll = create_linklist(title, date_created)
+        assert ll.title == title
+        assert ll.date_created == date_created
--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,6 @@
 pytest \
-    --ignore-glob='env/*.py' \
     --disable-pytest-warnings \
     --cov-report term-missing \
     --cov-branch \
     --cov-config=.coveragerc \
-    --cov=linksharing\
-    --cov=linklist\
-    --cov=users\
+    --cov=.
