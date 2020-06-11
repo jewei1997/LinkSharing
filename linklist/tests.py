c9917284197406fb46ad53e1eb4d670caeeb1f33
Jeremy Wei <jeremy.wei@flatiron.com>
2020-06-10 23:42:59 -0400

    Basic test now actually being run

--- linklist/tests.py
+++ linklist/tests.py
@@ -1,21 +1,17 @@
 from datetime import date
 from linklist.models import Link, LinkList
-from django.test import TestCase
 
 
 def create_linklist(title, date_created):
-    return LinkList.objects.create(title=title, date_created=date_created)
+    return LinkList(title=title, date_created=date_created)
 
 
 def create_link(title, link, date_added, linklist):
     return Link(title=title, link=link,
                 date_added=date_added, linklist=linklist)
 
-def test_hi():
-    assert 3 + 1 == 4
 
-
-class LinklistModelTests(TestCase):
+class TestLinklistModel:
     def test_create_linklist(self):
         title = "my list"
         date_created = date(2020, 6, 5)
