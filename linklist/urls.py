20b54e8e8db13cb786efea7901d86e5a59c22538
Jeremy Wei <jeremy.wei@flatiron.com>
2020-06-13 23:51:29 -0400

    `http http://127.0.0.1:8000/linklist/` works

--- linklist/urls.py
+++ linklist/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.linklists, name='index'),
     path('<int:linklist_id>/', views.detail, name='detail'),
 ]
--- linklist/views.py
+++ linklist/views.py
@@ -3,7 +3,35 @@ from django.views.generic import ListView
 from linklist.models import Link, LinkList
 from django.template import loader
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from linklist.serializers import LinkListSerializer
 
+
+# REST api stuff
+
+@api_view(['GET', 'POST', 'DELETE'])
+def linklists(request):
+    """
+    List the list of links a user has
+    """
+    if request.method == 'GET':
+        data = request.user.linklist_set.all() if request.user.is_authenticated else []
+        serializer = LinkListSerializer(data, context={'request': request}, many=True)
+        return Response({'data': serializer.data})
+    elif request.method == 'POST':
+        serializer = LinkListSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        serializer = LinkListSerializer(data=request.data)
+        serializer.delete()
+        return Response(status=status.HTTP._204_NO_CONTENT)
+
+"""
 # Index view for list of link lists
 def index(request):
     users_lists = request.user.linklist_set.all() if request.user.is_authenticated else []
@@ -12,6 +40,7 @@ def index(request):
             'lists': users_lists,
     }
     return HttpResponse(template.render(context, request))
+"""
 
 
 # Detail view for a list, which displays a list of links
